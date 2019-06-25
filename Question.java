e65ba3d6f9db2c0a1123f00c600e64a1d119a718
AlexBobryshev <darknessrider1@rambler.ru>
2019-06-25 19:50:48 +0300

    6th commit

--- Question.java
+++ Question.java
@@ -4,22 +4,22 @@ public class Question {
 	private int orig, transl, right;
 	private List<Integer> variants = new ArrayList<>();
 	
-	public Question(int num, Backend back) {
+	public Question(int num, Backend backend) {
 		boolean fit = false;
 		
 		orig = (int) (Math.random() * 2);
 		transl = 0;
 		if (orig == 0) transl = 1; //the language of translation and the language of original should be different
 		
-		right = (int) (Math.random() * back.getDifficulty());
-		for (int i = 0; i < back.getDifficulty(); i++) variants.add(-1); //so variants collection does not contain variants before the selection
+		right = (int) (Math.random() * backend.getDifficulty());
+		for (int i = 0; i < backend.getDifficulty(); i++) variants.add(-1); //so variants collection does not contain variants before the selection
 		variants.set(right, num);
 
-		for (int i = 0; i < back.getDifficulty(); i++) {
+		for (int i = 0; i < backend.getDifficulty(); i++) {
 			if (variants.get(i) == num) continue;
 			do {
 				do {
-					variants.set(i, (int) (Math.random() * back.getRange()));
+					variants.set(i, (int) (Math.random() * backend.getRange()));
 				} while (variants.get(i) == num); // check if the next generated variant is the same with the right
 				if (i != 0) {  // in order to avoid repetitions
 					for (int j = 0; j < i; j++) {
